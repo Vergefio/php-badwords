fa0711ddf41f656865444b15e6ce29e7c154350b
Vergefio <riccardods@live.it>
2022-11-30 16:08:08 +0100

    first commit

---
+++ index.php
@@ -0,0 +1,28 @@
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <meta charset="UTF-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <title>Bad Words PHP</title>
+    </head>
+
+    <body>
+
+        <?php 
+            $nome ="riccardo";
+            $insulto ="cazzo";
+        ?>
+
+        <form action="./pagina-di-atterraggio.php" method="GET">
+            <input type="text" name="nome" placeholder="nome">
+            <br>
+            <input type="text" name="censura" placeholder="censura">
+            <br>
+            <button type="submite">Invia</button>
+        </form>
+
+    </body>
+
+
+</html>
